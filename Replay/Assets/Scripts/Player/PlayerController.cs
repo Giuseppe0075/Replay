6c4dc1f5518499cefc2c0c5ff2ba35bc3271cbe6
Giuseppe Sica <giuseppesica03@gmail.com>
2023-08-11 17:38:32 +0200

    Player Controller

---
+++ Replay/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [Header("Movement")]
+    [SerializeField] float speed = 1f;
+    float xInput = 0;
+
+    [Header("Jump")]
+    [SerializeField] float jumpCut = 1f;
+    [SerializeField] float fallMultiplier;
+    [SerializeField] float jumpTime;
+    [SerializeField] float jumpForce = 10f;
+    float jumpCounter;
+    bool isJumping;
+
+    [Header("Controls")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayers;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            isJumping = true;
+            jumpCounter = 0;
+            rb.velocity = new Vector2(rb.velocity.x,jumpForce);
+        }
+
+        if(Input.GetButton("Jump") && isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCounter += Time.deltaTime;
+            if(jumpCounter > jumpTime)
+            {
+                isJumping = false;
+            }
+        }
+
+        if(Input.GetButtonUp("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x,rb.velocity.y / jumpCut);
+            isJumping = false;
+        }
+
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity -= new Vector2(rb.velocity.x, fallMultiplier) * Time.deltaTime;
+        }
+
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayers);
+    }
+}
